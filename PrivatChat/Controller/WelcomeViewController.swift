e74134b18041c19bd42bb474018d6e62d5daf7a5
Sergey Borisov <gransen@mail.ru>
2020-04-14 21:35:31 +0700

    Commit after adding brand colors

--- PrivatChat/Controller/WelcomeViewController.swift
+++ PrivatChat/Controller/WelcomeViewController.swift
@@ -13,23 +13,22 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = Constants.appName
-        
-//        titleLabel.text = ""
-//
-//        let titleText = "PrivatChat"
-//        var charIndex = 0.0
-//
-//        for letter in titleText {
-//
-//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-//                self.titleLabel.text?.append(letter)
-//            }
-//            charIndex += 1
-//        }
     }
 }
 
